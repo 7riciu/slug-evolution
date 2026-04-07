605d35b2d2e303e5bcdd7ddef8871383d96f3292
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2026-04-07 23:40:15 +0300

    Slugs leave collectable goo after them ^^

--- scripts/crate.gd
+++ scripts/crate.gd
@@ -2,14 +2,15 @@ extends Area2D
 
 @onready var game_scene = get_tree().get_first_node_in_group("game")
 var slug1_scene = preload("res://scenes/slug1.tscn")
+var slug1_instance = null
 
-func _ready() -> void:
-	self.position = Vector2(100, 30)
-	
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("click"):
 		$AnimatedSprite2D.play("open")
-		await get_tree().create_timer(1.0).timeout
-		var slug1_instance = slug1_scene.instantiate()
-		game_scene.add_child(slug1_instance)
-		slug1_instance.position = self.position + Vector2(10,10)
+		await get_tree().create_timer(0.5).timeout
+		if slug1_instance == null:
+			slug1_instance = slug1_scene.instantiate()
+			game_scene.add_child(slug1_instance)
+			slug1_instance.position = self.position + Vector2(10,10)
+			await get_tree().create_timer(0.5).timeout
+			self.queue_free()
