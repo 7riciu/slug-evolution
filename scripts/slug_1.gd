9d8a9a17112f761c5fe458fc7bdf9026e193f47f
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2026-04-09 15:39:59 +0300

    Added sprites

--- scripts/slug_1.gd
+++ scripts/slug_1.gd
@@ -3,18 +3,16 @@ extends Area2D
 @onready var game_scene = get_tree().get_first_node_in_group("game")
 var goo_scene = load("res://scenes/goo.tscn")
 var goo_instance = null
+var coin_scene = load("res://scenes/coin.tscn")
+var coin_instance = null
 
 func _ready() -> void:
 	spawn_goo()
 
-func _process(_delta: float) -> void:
-	if goo_instance != null:
-		if Input.is_action_just_pressed("click"):
-			goo_instance.queue_free()
-
 func spawn_goo():
 	while true:
 		if goo_instance == null:
+			print("goo")
 			goo_instance = goo_scene.instantiate()
 			goo_instance.position = self.position + Vector2(-50, 0)
 			game_scene.add_child(goo_instance)
